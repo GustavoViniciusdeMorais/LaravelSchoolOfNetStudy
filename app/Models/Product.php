70b1f695918e9e145860bec52cfbbd3f16b722e4
gustavoviniciusdemorais <gustavocop14dos@gmail.com>
2022-09-03 11:31:00 -0300

    accessors and mutators

---
+++ app/Models/Product.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description'];
+
+    public function getTitleAttribute($value)
+    {
+        return strtoupper($value);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = ucfirst($value);
+    }
+
+    /**
+     * Virtual short description attribute
+     * @return string
+     */
+    public function getShortDescriptionAttribute()
+    {
+        if(strlen($this->attributes['description']) > 10) {
+            return mb_substr($this->attributes['description'], 0, 10) . '...';
+        }
+        return $this->attributes['description'];
+    }
+}
